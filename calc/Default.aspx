c36709fc4eb5062b799b493ba2a6e22e8f9bb246
Octanitrogen <ndstone@email.neit.edu>
2017-03-31 11:05:25 -0400

    added neg/pos switch | percental | pi | squareroot

--- calc/Default.aspx
+++ calc/Default.aspx
@@ -5,15 +5,59 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <style>
+        input{
+            width:35px;
+            height:35px;
+        }
+        #txtLCD{
+            width:190px;
+            height:initial;
+        }
+        #btnClear{
+            width:100%;
+        }
+
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
     <div>
-    
-        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
-        <asp:Button ID="Button1" runat="server" Text="Button" />
-        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
-    
+        <asp:Label ID="lblOperand" runat="server" Text=""></asp:Label>
+        <asp:TextBox ID="txtLCD" runat="server">0</asp:TextBox>
+        <asp:Label ID="lblMemory" runat="server" Text=""></asp:Label>
+        <table>
+        <tr>
+            <td><asp:Button ID="butSMem" runat="server" Text="MS" OnClick="btnSMem_Click" /></td>
+            <td><asp:Button ID="butCMem" runat="server" Text="MC" OnClick="btnCMem_Click" /></td>
+            <td><asp:Button ID="butRMem" runat="server" Text="MR" OnClick="btnRMem_Click" /></td>
+            <td><asp:Button ID="btnPlus" runat="server" Text="+" OnClick="btnPlus_Click" /></td>
+            <td><asp:Button ID="btnNegPos" runat="server" Text="-/+" OnClick="btnNegPos_Click" /></td>
+        </tr><tr>
+            <td><asp:Button ID="btnNum7" runat="server" Text="7" OnClick="btnNum7_Click" /></td>
+            <td><asp:Button ID="btnNum8" runat="server" Text="8" OnClick="btnNum8_Click" /></td>
+            <td><asp:Button ID="btnNum9" runat="server" Text="9" OnClick="btnNum9_Click" /></td>
+            <td><asp:Button ID="btnMinus" runat="server" Text="-" OnClick="btnMinus_Click" /></td>
+            <td><asp:Button ID="btnPercent" runat="server" Text="%" OnClick="btnPercent_Click" /></td>
+        </tr><tr>
+            <td><asp:Button ID="btnNum4" runat="server" Text="4" OnClick="btnNum4_Click" /></td>
+            <td><asp:Button ID="btnNum5" runat="server" Text="5" OnClick="btnNum5_Click" /></td>
+            <td><asp:Button ID="btnNum6" runat="server" Text="6" OnClick="btnNum6_Click" /></td>
+            <td><asp:Button ID="btnMulti" runat="server" Text="X" OnClick="btnMulti_Click" /></td>
+            <td><asp:Button ID="btnPi" runat="server" Text="π" OnClick="btnPi_Click" /></td>
+        </tr><tr>
+            <td><asp:Button ID="btnNum1" runat="server" Text="1" OnClick="btnNum1_Click" /></td>
+            <td><asp:Button ID="btnNum2" runat="server" Text="2" OnClick="btnNum2_Click" /></td>
+            <td><asp:Button ID="btnNum3" runat="server" Text="3" OnClick="btnNum3_Click" /></td>
+            <td><asp:Button ID="btnDivide" runat="server" Text="÷" OnClick="btnDivide_Click" /></td>
+            <td><asp:Button ID="btnSquareR" runat="server" Text="√" OnClick="btnSquareR_Click" /></td>
+        </tr><tr>
+            <td><asp:Button ID="btnNum0" runat="server" Text="0" OnClick="btnNum0_Click" /></td>
+            <td><asp:Button ID="btnPoint" runat="server" Text="." OnClick="btnPoint_Click" /></td>
+            <td><asp:Button ID="btnEqual" runat="server" Text="=" OnClick="btnEqual_Click" /></td>
+            <td colspan="8"><asp:Button ID="btnClear" runat="server" Text="ON/C" OnClick="btnClear_Click" /></td>
+        </tr>
+        </table>
     </div>
     </form>
 </body>
--- calc/Default.aspx.cs
+++ calc/Default.aspx.cs
@@ -7,12 +7,277 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
+    public string rootCheck(string given)
+    {
+        if (given.Contains("√"))
+        {
+            double x = double.Parse(given.Substring(1));
+            given = Math.Sqrt(x).ToString();
+        } 
+        return given;
+    }
     protected void Page_Load(object sender, EventArgs e)
     {
 
     }
-    protected void Button1_Click(object sender, EventArgs e)
-    {
 
+    protected void btnNum0_Click(object sender, EventArgs e)
+    {
+        if (txtLCD.Text != "0")
+        {
+            txtLCD.Text += "0";
+        }
+    }
+    protected void btnNum1_Click(object sender, EventArgs e)
+    {
+        if (txtLCD.Text == "0")
+        {
+            txtLCD.Text = "1";
+        }
+        else
+        {
+            txtLCD.Text += "1";
+        }
+    }
+    protected void btnNum2_Click(object sender, EventArgs e)
+    {
+        if (txtLCD.Text == "0")
+        {
+            txtLCD.Text = "2";
+        }
+        else
+        {
+            txtLCD.Text += "2";
+        }
+    }
+    protected void btnNum3_Click(object sender, EventArgs e)
+    {
+        if (txtLCD.Text == "0")
+        {
+            txtLCD.Text = "3";
+        }
+        else
+        {
+            txtLCD.Text += "3";
+        }
+    }
+    protected void btnNum4_Click(object sender, EventArgs e)
+    {
+        if (txtLCD.Text == "0")
+        {
+            txtLCD.Text = "4";
+        }
+        else
+        {
+            txtLCD.Text += "4";
+        }
+    }
+    protected void btnNum5_Click(object sender, EventArgs e)
+    {
+        if (txtLCD.Text == "0")
+        {
+            txtLCD.Text = "5";
+        }
+        else
+        {
+            txtLCD.Text += "5";
+        }
+    }
+    protected void btnNum6_Click(object sender, EventArgs e)
+    {
+        if (txtLCD.Text == "0")
+        {
+            txtLCD.Text = "6";
+        }
+        else
+        {
+            txtLCD.Text += "6";
+        }
+    }
+    protected void btnNum7_Click(object sender, EventArgs e)
+    {
+        if (txtLCD.Text == "0")
+        {
+            txtLCD.Text = "7";
+        }
+        else
+        {
+            txtLCD.Text += "7";
+        }
+    }
+    protected void btnNum8_Click(object sender, EventArgs e)
+    {
+        if (txtLCD.Text == "0")
+        {
+            txtLCD.Text = "8";
+        }
+        else
+        {
+            txtLCD.Text += "8";
+        }
+    }
+    protected void btnNum9_Click(object sender, EventArgs e)
+    {
+        if (txtLCD.Text == "0")
+        {
+            txtLCD.Text = "9";
+        }
+        else
+        {
+            txtLCD.Text += "9";
+        }
+    }
+    protected void btnPoint_Click(object sender, EventArgs e)
+    {
+        if (!txtLCD.Text.Contains("."))
+        {
+            txtLCD.Text += ".";
+        }
+    }
+    protected void btnPlus_Click(object sender, EventArgs e)
+    {
+        Session["num1"] = rootCheck(txtLCD.Text);
+        Session["operand"] = "+";
+        txtLCD.Text = "0";
+        lblOperand.Text = "+";
+    }
+    protected void btnMinus_Click(object sender, EventArgs e)
+    {
+        Session["num1"] = rootCheck(txtLCD.Text);
+        Session["operand"] = "-";
+        txtLCD.Text = "0";
+        lblOperand.Text = "-";
+    }
+    protected void btnMulti_Click(object sender, EventArgs e)
+    {
+        Session["num1"] = rootCheck(txtLCD.Text);
+        Session["operand"] = "*";
+        txtLCD.Text = "0";
+        lblOperand.Text = "X";
+    }
+    protected void btnDivide_Click(object sender, EventArgs e)
+    {
+        Session["num1"] = rootCheck(txtLCD.Text);
+        Session["operand"] = "/";
+        txtLCD.Text = "0";
+        lblOperand.Text = "÷";
+    }
+    protected void btnEqual_Click(object sender, EventArgs e)
+    {
+        //declare answer variable
+        double answer = 0;
+
+        //check if there's a number to calcuate against
+        if (Session["num1"] != null)
+        {
+            Session["num2"] = rootCheck(txtLCD.Text);
+            double dblNum1 = Double.Parse(Session["num1"].ToString());
+            double dblNum2 = Double.Parse(Session["num2"].ToString());
+
+            //swtich case of the propper operations
+            string op = Session["operand"].ToString();
+            switch (op)
+            {
+                //addition
+                case "+":
+                    answer = dblNum1 + dblNum2;
+                    Session["num1"].Equals(null);
+                    Session["num2"].Equals(null);
+                    break;
+                //subtraction
+                case "-":
+                    answer = dblNum1 - dblNum2;
+                    Session["num1"].Equals(null);
+                    Session["num2"].Equals(null);
+                    break;
+                //multiplication
+                case "*":
+                    answer = dblNum1 * dblNum2;
+                    Session["num1"].Equals(null);
+                    Session["num2"].Equals(null);
+                    break;
+                //division
+                case "/":
+                    //if user decides to divide by 0
+                    if (Session["num2"].ToString() == "0")
+                    {
+                        txtLCD.Text = "ERR";
+                    }
+                    else
+                    {
+                        answer = dblNum1 / dblNum2;
+                        Session["num1"].Equals(null);
+                        Session["num2"].Equals(null);
+                    }
+                    break;
+            }
+            if (!txtLCD.Text.Contains("ERR"))
+            {
+                txtLCD.Text = answer.ToString();
+                lblOperand.Text = "";
+            }
+        }
+    }
+    protected void btnSMem_Click(object sender, EventArgs e)
+    {
+        Session["mem"] = txtLCD.Text;
+        lblMemory.Text = "M";
+        txtLCD.Text = "";
+    }
+    protected void btnCMem_Click(object sender, EventArgs e)
+    {
+        Session["mem"] = "";
+        lblMemory.Text = "";
+    }
+    protected void btnRMem_Click(object sender, EventArgs e)
+    {
+        txtLCD.Text = Session["mem"].ToString();
+    }
+    protected void btnClear_Click(object sender, EventArgs e)
+    {
+        Session["mem"] = null;
+        Session["num1"] = null;
+        Session["num2"] = null;
+        lblMemory.Text = "";
+        lblOperand.Text = "";
+        txtLCD.Text = "0";
+    }
+    protected void btnNegPos_Click(object sender, EventArgs e)
+    {
+        double given = double.Parse(txtLCD.Text);
+        if (given < 0)
+        {
+            given = given * -1;
+            txtLCD.Text = given.ToString();
+        }
+        else
+        {
+            txtLCD.Text = '-' + given.ToString();
+        }
+    }
+    protected void btnPercent_Click(object sender, EventArgs e)
+    {
+        //I don't know why I've added this button. This button is never reliable I find.
+        //Addition and subtraction make some sense I guess, multiplication and division is a mystery.
+
+        //check if there's a number to calcuate against
+        if (Session["num1"] != null)
+        {
+            double dblNum1 = Double.Parse(Session["num1"].ToString());
+            double dblNum2 = Double.Parse(txtLCD.Text);
+            dblNum2 = (dblNum1 * dblNum2) / 100;
+            txtLCD.Text = dblNum2.ToString();
+        }
+    }
+    protected void btnPi_Click(object sender, EventArgs e)
+    {
+        txtLCD.Text = Math.PI.ToString();
+    }
+    protected void btnSquareR_Click(object sender, EventArgs e)
+    {
+        if (txtLCD.Text == "0")
+        {
+            txtLCD.Text = "√";
+        }
     }
 }
